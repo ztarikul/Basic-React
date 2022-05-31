8e87c96e5202235c60b9fc100584619730098633
ztarikul <ztarikulislam@gmail.com>
2022-05-31 13:53:19 +0600

    controll form component added

--- src/components/representational/NewBook.js
+++ src/components/representational/NewBook.js
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 
-const NewBook = (props) => {
-    console.log(props);
-    return(
-        <div>
-            <h1>New Book Entry </h1>
-        </div>
-    )
+class NewBook extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            bookName: "",
+            writer: "",
+            description: ""
+        }
+    }
+
+
+    handleSubmit = (event) =>{
+        console.log(this.state);
+        event.preventDefault(); /// this data can be send to the serve;
+    }
+
+    handleInputChange = (event) =>{
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({
+            [name]: value
+        })
+
+    }
+
+
+    render(){
+        return(
+            <div>
+                <h1>New Book Entry </h1>
+                <form onSubmit={(event) => this.handleSubmit(event)}>
+                    <label>Book Name: </label>
+                    <br />
+                    <input type="text" name="bookName" value={this.state.bookName} onChange={(event) => this.handleInputChange(event)} />
+                    <br />
+                    <label>Writer: </label>
+                    <br />
+                    <input type="text" name="writer" value={this.state.writer} onChange={(event) => this.handleInputChange(event)} />
+                    <br />
+                    <label>Description: </label>
+                    <br />
+                    <textarea name="description" value={this.state.description} onChange={(event) => this.handleInputChange(event)} />
+                    <br />
+                    <input type="submit" value="Submit" />
+                    <br />
+                </form>
+            </div>
+        )
+    }
 }
 
 export default NewBook;
